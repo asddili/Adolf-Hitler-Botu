c817a5ca6879e141163311cc6db4b83f7df09ec3
asd <60388520+asddili@users.noreply.github.com>
2020-03-10 10:34:08 +0400

    Update main.js

--- main.js
+++ main.js
@@ -4,5 +4,11 @@ const client = new Discord.Client();
 client.once('ready', () => {
 	console.log('Ready!');
 });
+client.on('message', message => {
+	if (message.content === '!ping') {
+	// send back "Pong." to the channel the message was sent in
+	message.channel.send('Pong.');
+}
+});
 
 client.login('NjA2MTMwNTA2MzEyMDU2ODMz.XmZgbw.83jENvmBTfdiDCNyGpkPMmVny9I')
