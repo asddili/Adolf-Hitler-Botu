7a38be0819d0b10629af5b53eae772a4812857cf
asd <60388520+asddili@users.noreply.github.com>
2020-03-10 10:27:43 +0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('')NjA2MTMwNTA2MzEyMDU2ODMz.XmZgbw.83jENvmBTfdiDCNyGpkPMmVny9I
